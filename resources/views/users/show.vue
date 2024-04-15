0b91b0aafd752556b1d731f82e6e3cc29a5ada5c
Jaggy <jaggy@hey.com>
2024-04-15 14:17:55 +0800

    Recreate bug

---
+++ resources/views/users/show.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts">
+</script>
+
+<template>
+    <div>
+        I am a user
+    </div>
+</template>
+
+<style scoped>
+
+</style>
--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,19 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return hybridly('welcome');
 });
+
+Route::get('preload-users/1', function () {
+    return hybridly('users.show');
+})->name('preload.users.show');
+
+Route::post('preload-users/{user}', function ($id) {
+    dd("PRELOAD POST to {$id}");
+})->name('preload.users.update');
+
+Route::get('users/1', function () {
+    return hybridly('users.show');
+})->name('users.show');
+
+Route::post('users/{user}', function ($id) {
+    dd("NON PRELOAD POST to {$id}");
+})->name('users.update');
